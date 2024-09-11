cef1177e9b1736122f8ddc72252b7001261bcc13
MahmoudAladwy <mahmoudaladwy777@gmail.com>
2024-09-12 02:48:13 +0300

    create home repo

--- lib/Features/home/domain/entities/book_entity.dart
+++ lib/Features/home/domain/entities/book_entity.dart
@@ -5,5 +5,5 @@ class BookEntity {
   final num price;
   final num rating;
 
-  BookEntity({required this.image, required this.title, required this.authorName, required this.price, required this.rating});
+  BookEntity(this.image, this.title, this.authorName, this.price, this.rating);
 }
