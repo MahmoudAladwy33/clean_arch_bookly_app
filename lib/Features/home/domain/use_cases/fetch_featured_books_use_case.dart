292f22ae2136df88b636c2488483ca136dda95fe
MahmoudAladwy <mahmoudaladwy777@gmail.com>
2024-09-14 11:36:46 +0300

    create Fetch Featured Books Use Case

---
+++ lib/Features/home/domain/use_cases/fetch_featured_books_use_case.dart
@@ -0,0 +1,15 @@
+import 'package:bookly/Features/home/domain/entities/book_entity.dart';
+import 'package:bookly/Features/home/domain/repos/home_repo.dart';
+import 'package:bookly/core/errors/failures.dart';
+import 'package:dartz/dartz.dart';
+
+class FetchFeaturedBooksUseCase {
+  final HomeRepo homeRepo;
+
+  FetchFeaturedBooksUseCase(this.homeRepo);
+
+  Future<Either<Failure, List<BookEntity>>> fetchFeaturedBooks() {
+    //check permssion
+    return homeRepo.fetchFeaturedBooks();
+  }
+}
