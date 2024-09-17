d94a9f7b9b47fa274190c040a20c5dcb47519703
MahmoudAladwy <mahmoudaladwy777@gmail.com>
2024-09-17 17:55:50 +0300

    display featured books

--- lib/Features/home/presentation/views/widgets/featured_books_list_view_bloc_builder.dart
+++ lib/Features/home/presentation/views/widgets/featured_books_list_view_bloc_builder.dart
@@ -14,15 +14,22 @@ class FeaturedBooksListViewBlocBuilder extends StatelessWidget {
     return BlocBuilder<FeaturedBooksCubit, FeaturedBooksState>(
       builder: (context, state) {
         if (state is FeaturedBooksSuccess) {
-          return const FeaturedBooksListView();
+          return FeaturedBooksListView(
+            books: state.books,
+          );
         } else if (state is FeaturedBooksFailure) {
           return Center(
-              child: Text(
-            state.errMessage,
-            style: Styles.textStyle16,
-          ));
+            child: Text(
+              state.errMessage,
+              style: Styles.textStyle16,
+            ),
+          );
         } else {
-          return const CircularProgressIndicator();
+          return const SizedBox(
+            height: 10,
+            width: 10,
+            child: CircularProgressIndicator(),
+          );
         }
       },
     );
