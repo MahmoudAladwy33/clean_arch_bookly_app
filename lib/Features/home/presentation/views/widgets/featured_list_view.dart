d743a3515f9b0a1e31144e9a136a415a46dab379
MahmoudAladwy <mahmoudaladwy777@gmail.com>
2024-09-18 15:57:16 +0300

    fix pagination

--- lib/Features/home/presentation/views/widgets/featured_list_view.dart
+++ lib/Features/home/presentation/views/widgets/featured_list_view.dart
@@ -1,3 +1,5 @@
+import 'dart:developer';
+
 import 'package:bookly/Features/home/domain/entities/book_entity.dart';
 import 'package:bookly/Features/home/presentation/manger/featured_books_cubit/featured_books_cubit.dart';
 import 'package:flutter/material.dart';
@@ -19,6 +21,7 @@ class _FeaturedBooksListViewState extends State<FeaturedBooksListView> {
   late final ScrollController _scrollController;
 
   var nextPage = 1;
+  var isloading = false;
 
   @override
   void initState() {
@@ -27,13 +30,18 @@ class _FeaturedBooksListViewState extends State<FeaturedBooksListView> {
     _scrollController.addListener(_scrollListener);
   }
 
-  void _scrollListener() {
+  void _scrollListener() async {
     var currentPositions = _scrollController.position.pixels;
     var maxScrollLength = _scrollController.position.maxScrollExtent;
     if (currentPositions >= 0.7 * maxScrollLength) {
-      BlocProvider.of<FeaturedBooksCubit>(context)
-          .fetchFeaturedBooks(pageNumber: nextPage);
-      nextPage++;
+      if (!isloading) {
+        isloading = true;
+        await BlocProvider.of<FeaturedBooksCubit>(context)
+            .fetchFeaturedBooks(pageNumber: nextPage);
+        nextPage++;
+       
+        isloading = false;
+      }
     }
   }
 
